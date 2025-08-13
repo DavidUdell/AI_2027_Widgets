9eec4a564311fad77fab507cbde384a4f13f7703
DavidUdell <udelldavidb@gmail.com>
2025-08-13 17:59:41 +0300

    docs: note build changes

--- vite.config.js
+++ vite.config.js
@@ -12,18 +12,17 @@ export default defineConfig({
     base: './',
 });
 
-  // Library build
-  // {
-  //   build: {
-  //     outDir: 'dist',
-  //     emptyOutDir: false,
-  //     lib: {
-  //       entry: 'src/index.js',
-  //       name: 'AI_2027_Widgets',
-  //       formats: ['es', 'umd'],
-  //       fileName: (format) => `ai-2027-widgets.${format}.js`,
-  //     },
-  //     copyPublicDir: false,
-  //     sourcemap: true,
-  //   },
-  // }
+// Library build
+// {
+//   build: {
+//     outDir: 'dist',
+//     emptyOutDir: true,
+//     lib: {
+//       entry: 'src/index.js',
+//       name: 'AI_2027_Widgets',
+//       formats: ['es', 'umd'],
+//       fileName: (format) => `ai-2027-widgets.${format}.js`,
+//     },
+//     sourcemap: true,
+//   },
+// }
