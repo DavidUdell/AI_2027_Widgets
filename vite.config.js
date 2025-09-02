16b41d85daad6d4cd9085f9551344abb930f3d72
DavidUdell <udelldavidb@gmail.com>
2025-09-02 08:39:38 -0700

    chore: remove demo build

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,20 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Demo build
-    // build: {
-    //   outDir: 'dist',
-    //   emptyOutDir: true,
-    //   assetsDir: 'assets',
-    // },
-    // root: '.',
-    // publicDir: 'public',
-    // base: './',
-
-// Library build
-build: {
-  outDir: 'dist',
-  emptyOutDir: true,
-  lib: {
-    entry: 'src/index.js',
-    name: 'AI2027Widgets',
-    formats: ['es', 'iife'],
-    fileName: (format) => `ai-2027-widgets.${format}.js`,
-  },
-  sourcemap: true,
-  rollupOptions: {
-    output: {
-      inlineDynamicImports: true,
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    lib: {
+      entry: 'src/index.js',
+      name: 'AI2027Widgets',
+      formats: ['es', 'iife'],
+      fileName: (format) => `ai-2027-widgets.${format}.js`,
+    },
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
     },
   },
-},
 });
