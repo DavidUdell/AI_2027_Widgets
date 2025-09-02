bf9d75ed6a0956b37ffcdaea0e1727700d1abb45
DavidUdell <udelldavidb@gmail.com>
2025-09-02 08:00:54 -0700

    fix: switch to lib build

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,30 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Demo build
-    build: {
-      outDir: 'dist',
-      emptyOutDir: true,
-      assetsDir: 'assets',
-    },
-    root: '.',
-    publicDir: 'public',
-    base: './',
-});
+    // build: {
+    //   outDir: 'dist',
+    //   emptyOutDir: true,
+    //   assetsDir: 'assets',
+    // },
+    // root: '.',
+    // publicDir: 'public',
+    // base: './',
 
 // Library build
-// {
-//   build: {
-//     outDir: 'dist',
-//     emptyOutDir: true,
-//     lib: {
-//       entry: 'src/index.js',
-//       name: 'AI_2027_Widgets',
-//       formats: ['es', 'umd'],
-//       fileName: (format) => `ai-2027-widgets.${format}.js`,
-//     },
-//     sourcemap: true,
-//   },
-// }
+build: {
+  outDir: 'dist',
+  emptyOutDir: true,
+  lib: {
+    entry: 'src/index.js',
+    name: 'AI2027Widgets',
+    formats: ['es', 'iife'],
+    fileName: (format) => `ai-2027-widgets.${format}.js`,
+  },
+  sourcemap: true,
+  rollupOptions: {
+    output: {
+      inlineDynamicImports: true,
+    },
+  },
+},
+});
